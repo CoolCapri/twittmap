439104940a1a70ba345dc072bf64ff85601309cd
Shuo Wang <coolcaprimail@163.com>
2016-10-24 12:42:31 -0400

    working flask route to return json

--- application.py
+++ application.py
@@ -1,27 +1,38 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template
 from flask_socketio import SocketIO
 
-#from read_data import ReadData
+from read_data import DataReader
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 #application.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(application)
 
-#keywords = ["music, food, sport, tv, java, python, aws"]
-#data = ReadData()
-#tweets = data.read("static/data/tweets.txt", keywords)
-#for keyword in tweets.keys():
-#    print(len(tweets[keyword]))
+# pre-load fixed tweets
+def pre_load_fixed_data():
+    keywords = ["music", "food", "sport", "show", "movie", "car", "commercial", "party", "war", "hello"]
+    data = DataReader()
+    return data.read("static/data/tweets.txt", keywords)
+
+tweets_json = pre_load_fixed_data()
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/hello/')
-def hello():
-    return render_template('test1.html')
+@application.route('/searchf/')
+@application.route('/searchf/<keyword>')
+def searchf(keyword=None):
+    if keyword is None:
+        to_return = jsonify(**tweets_json)
+    else:
+        tweets_of_keyword = {keyword: []}
+        if keyword in tweets_json:
+            tweets_of_keyword = {keyword: tweets_json[keyword]}
+        to_return = jsonify(**tweets_of_keyword)
+    return to_return
 
 @socketio.on('message')
 def handle_message(message):
@@ -31,5 +42,5 @@ def handle_message(message):
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    #application.debug = True
+    application.debug = True
     application.run()
--- read_data.py
+++ read_data.py
@@ -1,4 +1,6 @@
-class ReadData:
+import json
+
+class DataReader:
 
     def read(self, filepath, keywords):
         data = {}
@@ -7,10 +9,10 @@ class ReadData:
 
         with open(filepath) as f:
             tweet_lines = f.readlines()
-            for i in range(0, 5):
-                line = tweet_lines[i]
-                print(line)
+            for i in range(0, len(tweet_lines)):
+                tweet_line = tweet_lines[i]
+                tweet_json = json.loads(tweet_line)
                 for keyword in keywords:
-                    if keyword in tweet_lines[i]:
-                        data[keyword].append(tweet_lines[i])
+                    if keyword in tweet_json['text'].lower():
+                        data[keyword].append(tweet_json)
         return data
