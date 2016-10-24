3b0ee501ead0f87d18b6b6312d73447f7603ae21
Shuo Wang <coolcaprimail@163.com>
2016-10-23 21:02:48 -0400

    add test page

--- application.py
+++ application.py
@@ -1,17 +1,31 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO
+
+from read_data import ReadData
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(application)
+
+keywords = ["music, food, sport, tv, java, python, aws"]
+data = ReadData()
+tweets = data.read("static/data/tweets.txt", keywords)
+for keyword in tweets.keys():
+    print(len(tweets[keyword]))
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
 @application.route('/hello/')
-@application.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello():
+    return render_template('test1.html')
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
 
 # run the app.
 if __name__ == "__main__":
